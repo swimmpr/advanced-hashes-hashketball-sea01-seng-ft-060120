ef597b9384df524f3f77638bf185a5ac74d38e8f
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-22 23:03:33 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -127,3 +129,95 @@ def game_hash
 end
 
 # Write code here
+
+def num_points_scored(player_search)
+  game_hash.each do | team, team_info|
+    team_info[:players].each do | player|
+      if player[:player_name] == player_search
+        return player[:points]
+      end
+    
+    end
+  end
+end
+
+def shoe_size(name)
+  game_hash.each do | team, team_info|
+    team_info[:players].each do | player|
+      if player[:player_name] == name
+        return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team_input)
+  if team_input == "Charlotte Hornets" 
+    return game_hash[:away][:colors]
+  else return game_hash[:home][:colors]
+  end
+end
+
+def team_names
+  game_hash.map do | team, team_info|
+    team_info[:team_name]
+  end
+end
+
+def player_numbers(input)
+  output = []
+  game_hash.each do | team, team_info|
+    if team_info[:team_name] == input 
+      team_info.each do | key, value |
+        if key == :players
+          value.each do | player |
+          output.push(player[:number])
+        end
+        end
+      end
+    end
+  end
+  return output
+end
+
+def player_stats(input)
+  game_hash.each do |team, team_info|
+    team_info.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if input == player[:player_name]
+            return player
+          end
+        end
+      end
+    end
+end
+end
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do | team, team_info|
+    team_info[:players].each do | player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
+
+# def player_helper
+#   game_hash[:home][:players].marge(game_hash)[:away][:players])
+# end
+
+
+# def num_points_scored(player)
+#   game_hash
+# end
+
+# def num_points_scored(player)
+#   player_helper[:name][:score]
+# end
+
